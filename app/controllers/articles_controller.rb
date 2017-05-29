6083f5e87f01e8f6ca0ac9d22ac9e45d2b20efeb
Alex Camacho <email": null>
2017-05-29 16:24:26 -0400

    Modify #create method to secure new article entry with private #articles_params method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,16 @@
 class ArticlesController < ApplicationController
   def new
-
   end
+
+  def create
+    @article = Article.new(articles_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :text)
+    end
 end
